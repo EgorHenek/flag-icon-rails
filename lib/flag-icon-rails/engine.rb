2a6d12d8a48812616b1d4ce4a6fc89ac7b27c7f9
henek <eagluhih@gmail.com>
2016-06-29 18:30:31 +0500

    First commit

--- lib/flag-icon-rails/engine.rb
+++ lib/flag-icon-rails/engine.rb
@@ -1,6 +1,11 @@
 module FlagIcon
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'flag-icon-rails.assets.precompile' do |app|
+        Dir.glob("#{root}/app/assets/images/flags/**/").each do |path|
+          app.config.assets.paths << path
+        end
+      end
     end
   end
 end
